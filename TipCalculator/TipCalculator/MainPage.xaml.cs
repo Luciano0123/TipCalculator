9017ef2aad644eff0341d8fbe3d8393ebd6c80a2
Luciano e Torbitoni <lgmoreno0802@gmail.com>
2024-03-19 12:38:32 -0300

    criação do tip

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button15Percent_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Button30Percent_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundUp_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundDown_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void PorcentagemSlider_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+        {
+            TipPercentualLabel.Text = $"{PorcentagemSlider.Value}%";
+
+        }
+    }
+
+}
